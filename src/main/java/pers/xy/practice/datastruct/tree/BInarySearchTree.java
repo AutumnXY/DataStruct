b643c52ee3d327d9c3c5c6470d20d9316a80e169
xy <zhangxinyu@elex-tech.com>
2020-08-21 18:31:14 +0800

    二叉树删除

--- src/main/java/pers/xy/practice/datastruct/tree/BInarySearchTree.java
+++ src/main/java/pers/xy/practice/datastruct/tree/BInarySearchTree.java
@@ -6,4 +6,62 @@ package pers.xy.practice.datastruct.tree;
  */
 public class BInarySearchTree {
 
+    private Node root;
+
+    class Node {
+        int val;
+        Node left;
+        Node right;
+    }
+
+
+    public void find(int num) {
+    }
+
+
+    public void insert(int num) {
+
+    }
+
+    public void delete(int num) {
+        if (root == null) {
+            return;
+        }
+        Node target = root, targetParent = null;
+        boolean isLeftChild = true;
+
+        //找到要删除的node
+        while (target != null) {
+            if (target.val == num) {
+                break;
+            }
+            if (num > root.val) {
+                target = root.right;
+                targetParent = target;
+                isLeftChild = false;
+            } else {
+                target = target.right;
+                targetParent = target;
+                isLeftChild = true;
+            }
+        }
+        if (target == null) {
+            System.out.println("没找到");
+        }
+
+        // 3种情况 叶子结点、有1个孩子的节点、有2个孩子的节点
+        if (target.left == null && target.right == null) { // 叶子结点
+            if (isLeftChild)
+                targetParent.left = null;
+            else
+                targetParent.right = null;
+        } else if (target.left == null || target.right == null) {//有1个孩子的节点
+            if (isLeftChild)
+                targetParent.left = target.left;
+        } else {
+
+        }
+    }
+
+
 }
