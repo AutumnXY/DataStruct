99147382221542b27de4ce63cfbb1ee1bcacb410
xy <zhangxinyu@elex-tech.com>
2020-08-24 21:35:55 +0800

    AVL树 插入、删除结点

--- src/main/java/pers/xy/practice/datastruct/tree/AVL.java
+++ src/main/java/pers/xy/practice/datastruct/tree/AVL.java
@@ -10,52 +10,195 @@ public class AVL {
 
     class Node {
         int val;
-        int height;
+        int height; // 高度
         Node left;
         Node right;
+
+        Node(int key) {
+            this.val = key;
+        }
     }
 
 
-    public void insert(Node insertNode) {
+    public void insert(int number) {
+        root = insert(root, number);
+    }
+
+    private Node insert(Node node, int number) {
+        if (node == null) {
+            node = new Node(number);
+        } else {
+            int compare = compare(node.val, number);
+            if (compare < 0) { // 往右添加
+                node.right = insert(node.right, number);
+                if (Math.abs(height(node.left) - height(node.right)) == 2) {// 需要调整
+                    if (number > node.right.val) {
+                        // RR型
+                        node = leftRotate(node);
+                    } else {
+                        //RL型
+                        node = rightLeftRotate(node);
+                    }
+                }
+            } else if (compare > 0) { // 往左添加
+                node.left = insert(node.left, number);
+                if (Math.abs(height(node.left) - height(node.right)) == 2) {// 需要调整
+                    if (number < node.left.val) {
+                        // LL型
+                        node = rightRotate(node);
+                    } else {
+                        // LR型
+                        node = leftRightRotate(node);
+                    }
+                }
+            } else {
+                System.out.println("error");
+            }
+        }
+        node.height = max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
+
+    private int max(int leftTreeHeight, int rightTreeHeight) {
+        return leftTreeHeight > rightTreeHeight ? leftTreeHeight : rightTreeHeight;
+    }
+
+    private int height(Node node) {
+        if (node == null)
+            return 0;
+
+        return node.height;
+    }
+
+    /**
+     * -1 a<b ;0 a==b ;1 a>b
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private int compare(int a, int b) {
+        if (a == b)
+            return 0;
+        return a > b ? 1 : -1;
+    }
+
+
+    public void remove(int number) {
+        // 先找到
+        Node toDel = search(number);
+        //如果没有就不删
+        if (toDel != null)
+            remove(root, toDel);
 
     }
 
-    public void delete(int val) {
+    private Node remove(Node node, Node toDel) {
+        if (node == null)
+            return null;
+
+        int compare = compare(node.val, toDel.val);
+        if (compare < 0) {// 去右面接着找删除点
+            node.right = remove(node.right, toDel);
+            if (Math.abs(height(node.left) - height(node.right)) == 2) {
+                Node l = node.left;
+                if (height(l.left) > height(l.right)) {
+                    node = leftRightRotate(node);
+                } else {
+                    node = (node);
+                }
+            }
+        } else if (compare > 1) {// 去左面接着找
+            node.left = remove(node.left, toDel);
+            if (Math.abs(height(node.left) - height(node.right)) == 2) {
+                Node r = node.right;
+                if (height(r.left) > height(r.right)) {
+                    node = rightRotate(node);
+                } else {
+                    node = rightLeftRotate(node);
+                }
+            }
+        } else { // 找到了，
+            // 正常二叉树删除逻辑
+            if (node.left != null && node.right != null) {// 有2个孩子
+                if (height(node.left) > height(node.right)) {
+                    Node leftTreeMax = findChildTreeMax(node.left);
+                    node.val = leftTreeMax.val;
+                    node.left = remove(node.left, leftTreeMax);
+                } else {
+                    Node rightTreeMin = findChildTreeMin(node.right);
+                    node.val = rightTreeMin.val;
+                    node.right = remove(node.right, rightTreeMin);
+                }
+            } else {
+                node = node.left == null ? node.right : node.left;
+            }
+        }
+
+        if (node != null)
+            node.height = max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
+
+    private Node findChildTreeMin(Node root) {
+
+        return null;
+    }
+
+    private Node findChildTreeMax(Node root) {
+        return null;
+    }
+
+    public Node search(int number) {
+        if (root == null)
+            return null;
+        return searchNode(root, number);
 
     }
 
-    public void balance(Node root) {
+    private Node searchNode(Node node, int number) {
 
+        int compare = compare(root.val, number);
+        if (compare < 0) {// 往右
+            searchNode(node.right, number);
+        } else if (compare > 0) {// 往左
+            searchNode(node.left, number);
+        } else {
+            return node;
+        }
+        return null;
     }
 
-    // LL
-    public void leftRotate(Node node) {
 
-        Node newParet = node.left;
-
-        node.left = newParet.right;
-
-        newParet.right = node;
-
+    // LL型
+    public Node rightRotate(Node node) {
+        Node newParent = node.left;
+        node.left = newParent.right;
+        newParent.right = node;
+        node.height = max(height(node.left), height(node.right)) + 1;
+        newParent.height = max(height(newParent.left), height(newParent.right)) + 1;
+        return newParent;
     }
 
-    // RR
-    public void rightRotate(Node node) {
-
+    // RR型
+    public Node leftRotate(Node node) {
         Node newParent = node.right;
-
         node.right = newParent.left;
-
         newParent.left = node;
-
+        node.height = max(height(node.left), height(node.right)) + 1;
+        newParent.height = max(height(newParent.left), height(newParent.right)) + 1;
+        return newParent;
     }
 
-    public void rightLeftRotate(Node node) {
-
-        leftRotate(node.right);
-
-        rightRotate(node);
+    // RL型对应的旋转
+    public Node rightLeftRotate(Node node) {
+        node.right = rightRotate(node.right);
+        return leftRotate(node);
     }
 
-
+    // LR型对应的旋转
+    public Node leftRightRotate(Node node) {
+        node.left = leftRotate(node.left);
+        return rightRotate(node);
+    }
 }
---
+++ src/test/java/pers/xy/practice/datastruct/tree/AVLTest.java
@@ -0,0 +1,23 @@
+package pers.xy.practice.datastruct.tree;
+
+/**
+ * @author xinyu
+ * @date 2020/8/24
+ */
+public class AVLTest {
+
+    private static int arr[] = {3, 2, 1, 4, 5, 6, 7, 16, 15, 14, 13, 12, 11, 10, 8, 9};
+
+
+    public static void main(String[] args) {
+        int i;
+        AVL tree = new AVL();
+
+        System.out.printf("== 依次添加: ");
+        for (i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+            tree.insert(arr[i]);
+        }
+
+    }
+}
