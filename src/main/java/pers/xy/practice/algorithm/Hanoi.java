e83533ce6c44d9fdfcad614a9aaf3dcdabc0eef4
xy <zhangxinyu@elex-tech.com>
2021-07-06 11:00:01 +0800

    hanoi

--- src/main/java/pers/xy/practice/algorithm/Hanoi.java
+++ src/main/java/pers/xy/practice/algorithm/Hanoi.java
@@ -9,11 +9,13 @@ package pers.xy.practice.algorithm;
 public class Hanoi {
 //  private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    int step = 0;
 
     public void hanoi(int n, String from, String to, String other) {
-        if (n == 1)
-            System.out.println(from + " -> " + to);
-        else {
+        if (n == 1) {
+            step++;
+//            System.out.println(from + " -> " + to);
+        } else {
             hanoi(n - 1, from, other, to);
             hanoi(1, from, to, other);
             hanoi(n - 1, other, to, from);
@@ -22,7 +24,10 @@ public class Hanoi {
 
 
     public static void main(String[] args) {
+        int n = 10;
         Hanoi hanoi = new Hanoi();
-        hanoi.hanoi(10, "A", "C", "B");
+        hanoi.hanoi(n, "A", "C", "B");
+        int expect = Double.valueOf(Math.pow(2, n)).intValue() - 1;
+        System.out.println("Step: " + hanoi.step + " == " + expect + " ? " + (hanoi.step == expect));
     }
 }
